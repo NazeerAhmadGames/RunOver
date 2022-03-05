cc8ca69587ebc699ce8845406d087c6bfa6ffeca
Fikriemre <Fikriemre@gmail.com>
2022-03-06 00:22:38 +0300

    fix

--- Assets/Scripts/CrowdSystem.cs
+++ Assets/Scripts/CrowdSystem.cs
@@ -159,6 +159,8 @@ public class CrowdSystem : MonoBehaviour
         {
             AddCharacter(character);
         }
+
+        CalculateFirstRow();
     }
 
     private void ControlForLevelFail()
